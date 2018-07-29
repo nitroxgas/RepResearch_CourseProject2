c6e1e889acf0f676d031db1d1510fa851066cd94
nitroxgas <nitroxgas@gmail.com>
2018-07-29 11:21:52 -0300

    new files

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-#rmarkdown::render("RepResearch_CourseProject2.Rmd")
+rmarkdown::render("RepResearch_CourseProject2.Rmd")
 #
 #healt_impact_injuries = storm_db %>% 
 ##  select(EVTYPE, INJURIES) %>% 
@@ -12,10 +12,16 @@
 #  summarize(events=n(), fatalities=sum(FATALITIES)) %>% 
 #  arrange(-fatalities)
 
-fatal_plot <- ggplot() + 
-  geom_bar( data = healt_impact_fatalities[1:10,], aes( x = reorder(EVTYPE, -EVTYPE), y = fatalities, fill = interaction(fatalities, EVTYPE)), stat = "identity", show.legend=F) +
-  xlab("Harmful Events") + 
-  ylab("No. of fatailities") + 
-  ggtitle("Top 5 weather events causing fatalities")
+#fatal_plot <- ggplot() + 
+#  geom_bar( data = healt_impact_fatalities[1:5,], aes( x = reorder(EVTYPE, -fatalities), y = fatalities, fill = interaction(fatalities, EVTYPE)), stat = "identity", show.legend=F) +
+#  xlab("Harmful Events") + 
+#  ylab("No. of fatailities") + 
+#  ggtitle("Top 5 weather events causing fatalities")
 
-grid.arrange(fatal_plot)
+#injuries_plot <- ggplot() + 
+#  geom_bar( data = healt_impact_injuries[1:5,], aes( x = reorder(EVTYPE, -injuries), y = injuries, fill = interaction(injuries, EVTYPE)), stat = "identity", show.legend=F) +
+#  xlab("Harmful Events") + 
+#  ylab("No. of fatailities") + 
+#  ggtitle("Top 5 weather events causing fatalities")
+
+#grid.arrange(fatal_plot, injuries_plot, ncol = 2)
